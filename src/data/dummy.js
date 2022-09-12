c1b15cb573f4ca5ce1c3711e319ebb7daf0177e8
Alvaro Guillén <alvaro.guillenc1612@gmail.com>
2022-09-12 14:29:53 -0500

    prima migration - update

--- src/data/dummy.js
+++ src/data/dummy.js
@@ -1,4 +1,4 @@
-const links = [{
+var links = [{
     id: 'link-0',
     url: 'www.howtographql.com',
     description: 'Fullstack tutorial for GraphQL'
--- src/lib/resolvers.js
+++ src/lib/resolvers.js
@@ -1,14 +1,20 @@
-import links from '../data/dummy.js'
+/* import links from '../data/dummy.js' */
+
+var links = [{
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    description: 'Fullstack tutorial for GraphQL'
+  }]
 
 const resolvers = {
     Query: {
         info: () => `API de alvaro`,
         feed: () => links,
         link: (parent, args) => {
+            
             const id = args.id;
-            links.filter((e) => {
-                if(e.id == id) return e;
-            })
+            const result = links.find(link => link.id == id)
+            return result;
         }
     },
     Mutation: {
@@ -24,7 +30,26 @@ const resolvers = {
           }
           links.push(link)
           return link
-        }
+        },
+        updateLink: (parent, args) => {
+            const {id, description, url} = args;
+            const linkIndex = links.findIndex(link => link.id == id) || -1 
+            
+            if(linkIndex >= 0) {
+                links[linkIndex].description = description
+                links[linkIndex].url = url
+                return links[linkIndex]
+            }
+        },
+/*         deleteLink: (parent, args) => {
+            const id = args.id
+            const linkIndex = links.findIndex(link => link.id == id) || -1 
+            
+            if(linkIndex >= 0) {
+                links = links.filter(link => link.id != links[linkIndex].id)
+                return links
+            }
+        } */
       },
 }
 
