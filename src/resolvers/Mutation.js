a7685414317696b7baef2e36ef746d25ea03b9fa
Alvaro <alvaro.guillenc1612@gmail.com>
2022-09-12 20:58:23 -0500

    reformateo

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -4,16 +4,11 @@ import jwt from 'jsonwebtoken'
 import {APP_SECRET, getUserId } from '../utils/authUtls.js'
 
 async function signup(parent, args, context, info) {
-    // 1
     const password = await bcrypt.hash(args.password, 10)
-  
-    // 2
     const user = await context.prisma.user.create({ data: { ...args, password } })
-  
-    // 3
+
     const token = jwt.sign({ userId: user.id }, APP_SECRET)
-  
-    // 4
+    
     return {
       token,
       user,
@@ -21,13 +16,11 @@ async function signup(parent, args, context, info) {
 }
 
 async function login(parent, args, context, info) {
-    // 1
     const user = await context.prisma.user.findUnique({ where: { email: args.email } })
     if (!user) {
       throw new Error('No such user found')
     }
   
-    // 2
     const valid = await bcrypt.compare(args.password, user.password)
     if (!valid) {
       throw new Error('Invalid password')
@@ -35,7 +28,6 @@ async function login(parent, args, context, info) {
   
     const token = jwt.sign({ userId: user.id }, APP_SECRET)
   
-    // 3
     return {
       token,
       user,
